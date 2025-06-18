f3523ec41374ebac0412ded48f50a3fc022774a8
miao-pqz <1125583676.qq.com>
2025-06-18 18:31:55 +0800

    4: add register form

--- schemas/index.ts
+++ schemas/index.ts
@@ -9,3 +9,16 @@ export const LoginSchema = z.object({
         .string()
         .min(1, { message: "Password is required"}),
 });
+
+export const RegisterSchema = z.object({
+    email: z
+        .string()
+        .nonempty("Email is required")
+        .email("Email is invalid"),
+    password: z
+        .string()
+        .min(6, { message: "Minimu 6 characters is required" }),
+    name: z
+        .string()
+        .min(1, { message: "Name is required" }),
+});
